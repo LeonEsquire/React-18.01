e8e2ed9fc036fbb4607a58774e085b092fc46dcc
ampersky86 <gasttello@gmail.com>
2019-02-07 12:15:44 +0300

    Танцы с git. Делаю Post на Flux с нуля

--- AleksandrAkperov/hw_6/src/app/pages/Posts.js
+++ AleksandrAkperov/hw_6/src/app/pages/Posts.js
@@ -1,37 +1,16 @@
 import React from 'react';
-import {addPost, getPosts} from "../actions/postActions";
-import PostStore from '../stores/postStore';
-
+import PostsList from "../components/PostsList";
 
 export default class Posts extends React.Component {
-    constructor (props){
-        super(props);
-        this.state = {
-            posts: []
-        }
-    }
-
-    onPostChange(){
-        this.setState({posts: PostStore.posts})
-    }
-
-    componentDidMount() {
-        getPosts();
-        PostStore.on('change', this.onPostChange);
-    }
-
-    newPost() {
-        const body = 'Текст поста';
-        const userId = 1;
-        const title = 'Мой пост';
-
-        addPost(title, userId, body);
-    }
-
     render() {
         return (
             <div>
-                <button className="btn btn-primary" onClick={this.newPost}>Добавить пост</button>
+                {
+                    (!this.props.children) ?
+                        (<PostsList/>)
+                        :
+                        (this.props.children)
+                }
             </div>
 
 
