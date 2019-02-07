8f8892f4e55968373bda00028eb8d88c645d6037
amper <gasttello@gmail.com>
2019-02-07 21:59:06 +0300

    Частично реализован функционал по добавлению постов через Flux

---
+++ AleksandrAkperov/hw_6/src/app/stores/postSrore.js
@@ -0,0 +1,49 @@
+import {ADD_POST, GET_POSTS} from "../constants/postConstants";
+import dispatcher from '../dispatcher';
+import {EventEmitter} from 'events';
+
+class postStore extends EventEmitter {
+    constructor ()
+    {
+        super(...arguments);
+        this.posts = [];
+        this.getPosts = this.getPosts.bind(this);
+        this.addPost = this.addPost.bind(this);
+        this.change = this.change.bind(this);
+    }
+
+
+    change ()
+    {
+        this.emit('change');
+    }
+
+
+    getPosts(posts) {
+        this.posts = posts;
+
+    }
+
+    addPost(post) {
+        this.posts = [post, ...this.posts];
+        this.change();
+    }
+
+
+    handleActions(action) {
+        switch (action.type){
+            case ADD_POST:
+                this.addPost(action.data);
+               break;
+            case GET_POSTS:
+                this.getPosts(action.data);
+                break;
+
+        }
+    }
+
+}
+
+const store = new postStore();
+dispatcher.register(store.handleActions);
+export default store;
