f0825a9f01256854af0eb108403e0a3dfeeea127
ampersky86 <gasttello@gmail.com>
2019-02-08 02:52:23 +0300

    this.getPosts is not a function - почему не функция, пока не понятно

--- AleksandrAkperov/hw_6/src/App.js
+++ AleksandrAkperov/hw_6/src/App.js
@@ -11,28 +11,23 @@ import PageNotFound from './app/pages/PageNotFound';*/
 
 import MainPage from './app/pages/Main';
 import Users from './app/pages/Users';
-import User from './app/pages/User';
+/*import User from './app/pages/User';*/
 import PageNotFound from './app/pages/PageNotFound';
 import Posts from './app/pages/Posts';
-import Post from './app/pages/Post';
-import Comments from './app/pages/Comments';
-import Comment from './app/pages/Comment';
+/*import Post from './app/pages/Post';*/
+/*import Comments from './app/pages/Comments';*/
+/*import Comment from './app/pages/Comment';*/
 
 
 ReactDOM.render (
     <Router history={browserHistory}>
         <Route path="/" component={Layout}>
             <IndexRoute component={MainPage} />
-            <Route path="users" component={Users}>
-              <Route path=":userId" component={User}/>
-            </Route>
-            <Route path="posts" component={Posts}>
-                <Route path=":id" component={Post}/>
-            </Route>
-            <Route path="comments" component={Comments}>
-                <Route path=":id" component={Comment}/>
-            </Route>
+            <Route path="users" component={Users}/>
+            <Route path="posts" component={Posts} />
+            {/*<Route path=":id" component={Posts}/>*/}
 
+        {/*<Route path="comments" component={Comments}/>*/}
             <Route path="*" component={PageNotFound}/>
         </Route>
     </Router>
--- AleksandrAkperov/hw_6/src/app/pages/Posts.js
+++ AleksandrAkperov/hw_6/src/app/pages/Posts.js
@@ -1,16 +1,41 @@
 import React from 'react';
+import {addPost, getPosts} from "../actions/postActions";
+import PostStore from '../stores/postStore';
 import PostsList from "../components/PostsList";
 
 export default class Posts extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            posts: []
+        };
+        this.onPostChange = this.onPostChange.bind(this);
+        this.newPost = this.newPost.bind(this);
+
+    }
+
+    onPostChange () {
+        this.setState({posts: PostStore.posts});
+    }
+
+    componentDidMount() {
+        getPosts();
+        PostStore.on('on', this.onPostChange);
+
+    }
+
+    newPost (){
+        const body = 'Я написал эти строки, я молодец ';
+        const userId = 1;
+        const title = 'Пост_1';
+        addPost(title, userId, body);
+    }
+
     render() {
         return (
             <div>
-                {
-                    (!this.props.children) ?
-                        (<PostsList/>)
-                        :
-                        (this.props.children)
-                }
+                <button className="btn btn-primary" onClick={this.newPost}>Добавить пост</button>
+                <PostsList posts = {this.state.posts} />
             </div>
 
 
