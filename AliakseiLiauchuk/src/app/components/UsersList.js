d65f912cb8b76101a87bc7cbe3b59c9e696b161a
headmaster11 <headmaster11@yandex.ru>
2019-02-08 16:01:56 +0300

    task 1

--- AliakseiLiauchuk/src/app/components/UsersList.js
+++ AliakseiLiauchuk/src/app/components/UsersList.js
@@ -1,27 +1,13 @@
 import React from 'react';
-import axios from 'axios';
 import User from './User';
 
 export default class UsersList extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      users: []
-    };
-    
-    axios.get('https://jsonplaceholder.typicode.com/users/')
-    .then(response => {
-      this.setState({users: response.data})
-    });
-  }
-
   render() {
-    if (!this.state.users.length) {
+    if (!this.props.users.length) {
       return null;
     }
 
-    const users = this.state.users.map((user, index) => {
+    const users = this.props.users.map((user, index) => {
       return <User key={index} {...user} />
     });
 
