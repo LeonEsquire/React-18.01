811b0faa2f047085c6de59043b72633608811e6b
headmaster11 <headmaster11@yandex.ru>
2019-02-11 17:52:37 +0300

    moved to axios

--- AliakseiLiauchuk/src/actions/index.js
+++ AliakseiLiauchuk/src/actions/index.js
@@ -1,4 +1,5 @@
 export const RECEIVE_USERS = 'RECEIVE_USERS'
+import axios from 'axios'
 
 export const receiveUsers = (json) => ({
   type: RECEIVE_USERS,
@@ -6,7 +7,7 @@ export const receiveUsers = (json) => ({
 })
 
 export const fetchUsers = () => dispatch => {
-  return fetch(`https://jsonplaceholder.typicode.com/users`)
-    .then(response => response.json())
+  return axios.get(`https://jsonplaceholder.typicode.com/users`)
+    .then(response => response.data)
     .then(json => dispatch(receiveUsers(json)))
 }
