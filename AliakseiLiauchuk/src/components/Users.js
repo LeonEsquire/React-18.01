5bd9a7de53194ac8a5879430d87c148833250bc6
headmaster11 <headmaster11@yandex.ru>
2019-02-11 18:08:00 +0300

    refactor user component

--- AliakseiLiauchuk/src/components/Users.js
+++ AliakseiLiauchuk/src/components/Users.js
@@ -3,14 +3,14 @@ import React from 'react'
 const Users = ({users}) => (
 
   <ul>
-    {Array.prototype.map.call(users, (user, index) => {
+    {users.map((user, index) => {
       return <li key={index}>
-          <p>{user.username}</p>
-          <p>{user.name}</p>
-          <p>{user.email}</p>
-          <p>{user.phone}</p>
-      </li>;
-    }, this)}
+        <p>{user.username}</p>
+        <p>{user.name}</p>
+        <p>{user.email}</p>
+        <p>{user.phone}</p>
+      </li>
+    })}
   </ul>
 )
 
