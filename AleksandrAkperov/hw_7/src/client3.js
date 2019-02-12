d61bdb054b9aa8469219bf18fb2b2fcf3de54c25
ampersky86 <gasttello@gmail.com>
2019-02-12 11:27:21 +0300

    fix_1

--- AleksandrAkperov/hw_7/src/client3.js
+++ AleksandrAkperov/hw_7/src/client3.js
@@ -9,7 +9,7 @@ const initialState = {
     users: [],
     error: null
 }
-const reducer = (state={}, action) => {
+const reducer = (state=initialState, action) => {
     switch (action.type) {
         case "FETCH_USER_START":{
             return {...state, fetching: true}
