03c897c2554ef044e98f07af13742e54450b35de
headmaster11 <headmaster11@yandex.ru>
2019-02-12 15:07:18 +0300

    add Menu component

--- AliakseiLiauchuk/src/components/Layout.js
+++ AliakseiLiauchuk/src/components/Layout.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Menu from './Menu'
 import MenuItem from './MenuItem'
 
 export default class Layout extends React.Component {
@@ -14,21 +15,14 @@ export default class Layout extends React.Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <div className="container">
-            <a href="/" className="navbar-brand">{this.props.brand}</a>
-            <div className="collapse navbar-collapse">
-              <ul className="navbar-nav">
-                <MenuItem href="/" active={this.isActive('/')}>
-                  Главная
-                </MenuItem>
-                <MenuItem href="/users" active={this.isActive('/users')}>
-                  Пользователи
-                </MenuItem>
-              </ul>
-            </div>
-          </div>
-        </nav>
+        <Menu brand={this.brand}>
+          <MenuItem href="/" active={this.isActive('/')}>
+            Главная
+          </MenuItem>
+          <MenuItem href="/users" active={this.isActive('/users')}>
+            Пользователи
+          </MenuItem>
+        </Menu>
         <div className="container">
           <div className="row">
             <div className="col-12">
---
+++ AliakseiLiauchuk/src/components/Menu.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default class Menu extends React.Component {
+    render() {
+        return         <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container">
+          <a href="/" className="navbar-brand">{this.props.brand}</a>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav">
+              {this.props.children}
+            </ul>
+          </div>
+        </div>
+      </nav>
+    }
+}
