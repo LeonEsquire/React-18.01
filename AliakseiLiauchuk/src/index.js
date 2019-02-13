70ad004c0742894414343e0a5517c66732107d43
headmaster11 <headmaster11@yandex.ru>
2019-02-13 16:12:01 +0300

    refactor importing

--- AliakseiLiauchuk/src/index.js
+++ AliakseiLiauchuk/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render } from 'react-dom'
+import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
@@ -7,7 +7,7 @@ import { createLogger } from 'redux-logger'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Switch, Route } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
-import reducer from './reducers'
+import reducers from './reducers'
 import Users from './containers/Users'
 import PageNotFound from './pages/PageNotFound'
 import MainPage from './pages/Main'
@@ -19,11 +19,11 @@ const middleware = [ thunk ]
 // }
 
 const store = createStore(
-  reducer,
+  reducers,
   applyMiddleware(...middleware)
 )
 
-render(
+ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
 		  <Layout>
