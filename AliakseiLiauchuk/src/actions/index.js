d56a0a5153cd1f07781710e54ba9e160ac27da00
headmaster11 <headmaster11@yandex.ru>
2019-02-13 13:42:06 +0300

    refactoring

--- AliakseiLiauchuk/src/actions/index.js
+++ AliakseiLiauchuk/src/actions/index.js
@@ -2,29 +2,30 @@ export const RECEIVE_USERS = 'RECEIVE_USERS'
 export const ADD_USER = 'ADD_USER'
 import axios from 'axios'
 
-export const receiveUsers = json => ({
-  type: RECEIVE_USERS,
-  users: json,
-})
 
-export const addUser = users => ({
-  type: ADD_USER,
-  user: {
-    username: "newUsername",
-    name: "newName",
-    email: "newEmail",
-    phone: "newPhone"
-  },
-  users: users
-})
-
-export const fetchUsers = () => dispatch => {
+export const getUsers = () => dispatch => {
   return axios.get(`https://jsonplaceholder.typicode.com/users`)
     .then(response => response.data)
-    .then(json => dispatch(receiveUsers(json)))
+    .then(json => dispatch(
+      {
+        type: RECEIVE_USERS,
+        users: json,
+      }
+      ))
 }
 
-export const fetchUser = () => (dispatch, getState) => {
+export const addUser = () => (dispatch, getState) => {
   const users = getState()
-  return dispatch(addUser(users))
+  return dispatch(
+    {
+      type: ADD_USER,
+      user: {
+        username: "newUsername",
+        name: "newName",
+        email: "newEmail",
+        phone: "newPhone"
+      },
+      users: users
+    }
+  )
 }
--- AliakseiLiauchuk/src/containers/Users.js
+++ AliakseiLiauchuk/src/containers/Users.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { fetchUsers, fetchUser } from '../actions'
+import { getUsers, addUser } from '../actions'
 import { UsersList } from '../components/UsersList'
 
 class Users extends Component {
-  fetchUser() {
-    this.props.dispatch(fetchUser());
+  addUser() {
+    this.props.dispatch(addUser());
   }
 
   componentDidMount() {
     const { dispatch } = this.props
-    dispatch(fetchUsers())
+    dispatch(getUsers())
   }
 
   render() {
     const { users } = this.props
     return (<>
-              <button type="submit" onClick={this.fetchUser.bind(this)}>add user</button>
+              <button type="submit" onClick={this.addUser.bind(this)}>add user</button>
               <UsersList users={users} />
             </>
     )
