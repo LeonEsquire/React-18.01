89a648722409a249884dd93ccd094ad5a1eea16f
ampersky86 <gasttello@gmail.com>
2019-02-15 12:06:08 +0300

    Танцы с git

---
+++ AleksandrAkperov/hw_8/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Todo from "./src/Todo";
+import Todo from "./src/Todo";
+import Todo from "./src/Todo";
+
+
+
+const app = document.querySelector('#root');
+
+
+ReactDOM.render(<Todo/>, app)
