22c8a666e8ded4626fbef0fa758c480997405973
ampersky86 <gasttello@gmail.com>
2019-02-15 11:44:58 +0300

    Не могу вытолкнуть users в html

--- AleksandrAkperov/hw_7/src/app/layouts/Layout.js
+++ AleksandrAkperov/hw_7/src/app/layouts/Layout.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
+import {fetchUser} from "../actions/userActions";
 
  class Layout extends React.Component {
+
+    
+
     render() {
-        console.log(this.props)
-        return (
-            null
-        );
+        console.log(this.props);
+        return <h1> 1 </h1>   
     }
+
+    componentDidMount(){
+        this.props.dispatch(fetchUser());
+    }
+
 }
 
 function mapStateToProps(state) {
 
     return {
-        
-        user:state.user.user
+        user:state.user.users,
+        userFetch:state.user.fetched,
+        tweets: state.tweets.tweets
     }
 }
 
-export default connect(mapStateToProps)(Layout)
+export default connect(mapStateToProps)(Layout);
