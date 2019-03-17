161890cbf88a7f88f99f5cbf193f6426374359c3
ampersky86 <gasttello@gmail.com>
2019-03-17 19:25:29 +0300

    fix_1

--- AleksandrAkperov/hw_8/src/app/pages/MainPage.js
+++ AleksandrAkperov/hw_8/src/app/pages/MainPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const MainPage = props => (
     <div>
         <h1>Главная страница</h1>
-        <p>Добро пожаловать</p>
+        <p>Добро пожаловать это 8 ветка</p>
     </div>
 );
 
